bb450dd0a991687ddfe26550f6cd8a1b9c453af2
Ben Sinther <b.sinther@sap.com>
2014-04-06 09:33:14 +0200

    leere Abhängigkeiten Liste in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('billomatInvoiceDownloader')
+angular.module('billomatInvoiceDownloader',[])
   .controller('MainCtrl', function($scope, $http, localStorageService) {
     
     $scope.resource = 'invoices';
