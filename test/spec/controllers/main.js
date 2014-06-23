d22e78170d4570dde147a5bb658ef5de7380fcd2
ilovemygameboy <bensinther@gmail.com>
2014-06-23 13:20:16 +0200

    - big changes: 	- new icon 	- new ui 	- new favicon 	- complete bootstrap layout

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,6 +19,6 @@ describe('Controller: MainCtrl', function () {
   }));
 
  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.awesomeThings.length).toBe(42);
   });
 });
